5922348acd0b4a86527aaad3a361e6f9eca3e840
nanigasi_san <nanigasi.py@gmail.com>
2021-11-01 14:48:57 +0900

    add base program

---
+++ Untitled-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    unsigned char *header, *data;
+    char input_file[100], output_file[100];
+    long start_addr, end_addr;
+    int width, height;
+}
